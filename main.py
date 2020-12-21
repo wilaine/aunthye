586a71c5114508b5f0db9716091b200acc345036
Cyber.W <43579453+wilaine@users.noreply.github.com>
2020-12-21 21:02:42 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+
+    
+def main():
+        
+        chk = True
+        while chk:
+                print("**********************************")
+                print("1. List                                        *")
+                print("2. Search by gender              *")
+                print("3. Search by one keyword         *")
+                print("4. Search by two keywords        *")
+                print("5. Update                        *")
+                print("6. Add                           *")
+                print("7. Delete                        *")
+                print("8. Exit                          *")
+                print("**********************************")
+option = input("Please enter an option ")
+if option == "1":
+        read()
+elif option == "2":
+        find_gender()
+elif option == "3":
+        oneKeyword()                
+elif option == "4":
+        twoKeyword()
+elif option == "5":
+        update()        
+elif option == "6":
+        append()        
+elif option == "4":
+        delete()        
+elif option == "4":
+        exit()                        
+else:
+        print("Not Valid option")
+repeat = input("Continue? (y/n) ")
+if repeat == 'n':
+        chk = False
