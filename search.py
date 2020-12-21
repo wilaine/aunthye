dbf50ccb94f110ada11fc7c5240eb51e3b7b2175
Cyber.W <43579453+wilaine@users.noreply.github.com>
2020-12-21 21:05:16 +0800

    Create search.py

---
+++ search.py
@@ -0,0 +1,21 @@
+def search():
+    import csv
+    with open('python.csv', 'r') as file:
+        count = 1
+        reader = csv.reader(file)
+        ans = input("Enter the gender to search (male or female) : ")
+        print('{:<3} {:<33}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format("No","Name","myKIDNumber","Guardian Name","Relationship","City","Phone Number","Car Plate"))
+        for row in reader:
+            gender = int(row[1].split('-')[2])
+            if(ans=="male"):
+                if(gender%2==0):
+                    continue
+                else:
+                    print(count,' {:<34}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format(*row))
+                    count +=1  
+            elif(ans=="female"):
+                if(gender%2==0):
+                    print(count,' {:<34}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format(*row))
+                    count +=1 
+            else:
+                print("Enter male or female!")
