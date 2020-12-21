1d0b5311c803dd9b5804d459e7fa81373ee65069
Cyber.W <43579453+wilaine@users.noreply.github.com>
2020-12-21 21:25:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from functions import read
 from functions import find_gender
 from functions import oneKeyword
+from functions import twoKeyword
+from functions import update
+from functions import append
+from functions import delete
+from functions import exit
     
 def main():
         
@@ -29,9 +34,9 @@ elif option == "5":
         update()        
 elif option == "6":
         append()        
-elif option == "4":
+elif option == "7":
         delete()        
-elif option == "4":
+elif option == "8":
         exit()                        
 else:
         print("Not Valid option")
