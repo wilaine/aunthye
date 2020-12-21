781eb88ddee5de176d0e5f20646bf515bdfd8bd1
Cyber.W <43579453+wilaine@users.noreply.github.com>
2020-12-21 21:49:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from functions import read
 from functions import find_gender
-from functions import oneKeyword
+"""from functions import oneKeyword
 from functions import twoKeyword
 from functions import update
 from functions import append
 from functions import delete
-from functions import exit
+from functions import exit """
     
 def main():
         
@@ -21,25 +21,25 @@ def main():
                 print("7. Delete                        *")
                 print("8. Exit                          *")
                 print("**********************************")
-option = input("Please enter an option ")
-if option == "1":
-        read()
-elif option == "2":
-        find_gender()
-elif option == "3":
-        oneKeyword()                
-elif option == "4":
-        twoKeyword()
-elif option == "5":
-        update()        
-elif option == "6":
-        append()        
-elif option == "7":
-        delete()        
-elif option == "8":
-        exit()                        
-else:
-        print("Not Valid option")
-repeat = input("Continue? (y/n) ")
-if repeat == 'n':
-        chk = False
+                option = input("Please enter an option ")
+                if option == "1":
+                        read()
+                elif option == "2":
+                        find_gender()
+                elif option == "3":
+                        oneKeyword()                
+                elif option == "4":
+                        twoKeyword()
+                elif option == "5":
+                        update()        
+                elif option == "6":
+                        append()        
+                elif option == "7":
+                        delete()        
+                elif option == "8":
+                        exit()                        
+                else:
+                        print("Not Valid option")
+                repeat = input("Continue? (y/n) ")
+                if repeat == 'n':
+                        chk = False
