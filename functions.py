40c3180fa0d31bcb9f2ffaa14d1ca6cb88f68a7e
Cyber.W <43579453+wilaine@users.noreply.github.com>
2020-12-21 23:29:32 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -31,9 +31,21 @@ def find_gender():
             else:
                 print("Enter male or female!")
                 
-"""def oneKeyword():
+def oneKeyword():
+    import csv
+    with open('children_record.csv', 'r') as file:
+        count = 1
+        reader = csv.reader(file)
+        print('{:<3} {:<33}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format("No","Name","myKIDNumber","Guardian Name","Relationship","City","Phone Number","Car Plate"))
+        key = input("Enter a keyword to search")
+        filtered = filter(lambda p: key )
+        for row in reader:
+            
+            
+            print(count," ",' {:<34}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format(*row))
+            count +=1
 
-def twoKeyword():
+"""def twoKeyword():
 
 def update():
 
@@ -42,3 +54,16 @@ def append():
 def delete():
     
 def exit():"""
+
+
+class color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
