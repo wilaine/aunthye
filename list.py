c7fbdf952ec62b9fbba6c23cb6755cb5092e052d
Cyber.W <43579453+wilaine@users.noreply.github.com>
2020-12-21 21:13:21 +0800

    Delete list.py

--- list.py
+++
@@ -1,9 +0,0 @@
-def list():
-    import csv
-    with open('python.csv', 'r') as file:
-        count = 1
-        reader = csv.reader(file)
-        print('{:<3} {:<33}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format("No","Name","myKIDNumber","Guardian Name","Relationship","City","Phone Number","Car Plate"))
-        for row in reader:
-            print(count,' {:<34}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format(*row))
-            count +=1      
