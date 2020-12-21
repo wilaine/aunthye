f519cde5e948a979de220e2f4af15c9955caffb7
Cyber.W <43579453+wilaine@users.noreply.github.com>
2020-12-21 23:29:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from functions import read
 from functions import find_gender
-"""from functions import oneKeyword
-from functions import twoKeyword
+from functions import oneKeyword
+"""from functions import twoKeyword
 from functions import update
 from functions import append
 from functions import delete
@@ -43,3 +43,5 @@ def main():
         repeat = input("Continue? (y/n) ")
         if repeat == 'n':
                 chk = False
+
+main()
