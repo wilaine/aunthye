e1781b6528390650e99d0829ba5757166824293e
Cyber.W <43579453+wilaine@users.noreply.github.com>
2020-12-26 15:29:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from functions import read
 from functions import find_gender
-from functions import oneKeyword
+from onekey import oneKeyword
 """from functions import twoKeyword
 from functions import update
 from functions import append
