5e0b67fd47ab245ede64a4aa9767a888c1dc9b6f
Cyber.W <43579453+wilaine@users.noreply.github.com>
2020-12-26 15:29:28 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,8 +3,9 @@ def read():
     with open('children_record.csv', 'r') as file:
         count = 1
         reader = csv.reader(file)
-        print('{:<3} {:<33}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format("No","Name","myKIDNumber","Guardian Name","Relationship","City","Phone Number","Car Plate"))
         for row in reader:
+            if(count==1):
+                print('{:<3} {:<33}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format("No","Name","myKIDNumber","Guardian Name","Relationship","City","Phone Number","Car Plate"))
             print(count,' {:<34}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format(*row))
             count +=1      
 
@@ -14,36 +15,27 @@ def find_gender():
         count = 1
         reader = csv.reader(file)
         ans = input("Enter the gender to search (male or female) : ")
-        if(ans=="male" or ans=="female"):
-            print('{:<3} {:<33}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format("No","Name","myKIDNumber","Guardian Name","Relationship","City","Phone Number","Car Plate"))
         for row in reader:
-            gender = int(row[1].split('-')[2])
-            if(ans=="male"):
-                if(gender%2==0):
-                    continue
-                else:
-                    print(count,' {:<34}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format(*row))
-                    count +=1  
-            elif(ans=="female"):
-                if(gender%2==0):
-                    print(count,' {:<34}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format(*row))
+            if(ans=="male" or ans=="female"):
+                if(count==1):
+                    print('{:<3} {:<33}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format("No","Name","myKIDNumber","Guardian Name","Relationship","City","Phone Number","Car Plate"))
+                gender = int(row[1].split('-')[2])
+                if(ans=="male"):
+                    if(gender%2==0):
+                        continue
+                    else:
+                        print(count,' {:<34}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format(*row))
+                        count +=1  
+                elif(ans=="female"):
+                    if(gender%2==0):
+                        print(count,' {:<34}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format(*row))
                     count +=1 
             else:
-                print("Enter male or female!")
+                print("Please try again!! Enter male or female!")
+                count +=1
+                break
                 
-def oneKeyword():
-    import csv
-    with open('children_record.csv', 'r') as file:
-        count = 1
-        reader = csv.reader(file)
-        print('{:<3} {:<33}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format("No","Name","myKIDNumber","Guardian Name","Relationship","City","Phone Number","Car Plate"))
-        key = input("Enter a keyword to search")
-        filtered = filter(lambda p: key )
-        for row in reader:
-            
-            
-            print(count," ",' {:<34}  {:<15}  {:<25} {:<15} {:<15} {:<15} {:<15}'.format(*row))
-            count +=1
+
 
 """def twoKeyword():
 
